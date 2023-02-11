a897ba10560805a3ff309d0669315c342f2341d7
roqaya <roqayaabumealiq@gmail.com>
2023-02-11 09:10:42 +0200

    first commit

---
+++ app/src/main/java/com/r/m/m/my_first_app_cloud/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.r.m.m.my_first_app_cloud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
